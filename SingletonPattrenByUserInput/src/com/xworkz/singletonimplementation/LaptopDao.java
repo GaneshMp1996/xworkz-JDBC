ac0cba88bc98db1f73a7ec67ed6e9b57faf9b9c5
ganeshmp1996 <[ganeshmpganesh3@gmail.com]>
2021-10-17 10:11:21 -0700

    Push

--- SingletonPattrenByUserInput/src/com/xworkz/singletonimplementation/LaptopDao.java
+++ SingletonPattrenByUserInput/src/com/xworkz/singletonimplementation/LaptopDao.java
@@ -1,7 +1,8 @@
 package com.xworkz.singletonimplementation;
 
 public interface LaptopDao {
-	void savelaptopRecord(int id,String brand, String color,double price);
+	void savelaptopRecord(int id, String brand, String color, double price, int ram, String processor,
+			boolean Is_Camera_Present,String Screen_Size, int Weight, boolean Is_Cd_Supported);
 
 	void updateBrand(String updateBrand);
 
@@ -9,12 +10,14 @@ public interface LaptopDao {
 
 	void readSingleRecordById(int id);
 
-	void readAllRecordById(int id);
+	void readAllRecord();
 
 	void readlaptopPriceById(int id);
 
 	void readLaptopBrandById(int id);
 
 	void readLaptopColorById(int id);
+
+	
 
 }
