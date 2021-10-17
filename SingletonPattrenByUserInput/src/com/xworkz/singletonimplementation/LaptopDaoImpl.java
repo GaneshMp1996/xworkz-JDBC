541a7d359adc553d23175f8a3a47b55ce3d2aa3a
ganeshmp1996 <[ganeshmpganesh3@gmail.com]>
2021-10-17 08:57:30 -0700

    Singleton Pattern Implementation By User Input

--- SingletonPattrenByUserInput/src/com/xworkz/singletonimplementation/LaptopDaoImpl.java
+++ SingletonPattrenByUserInput/src/com/xworkz/singletonimplementation/LaptopDaoImpl.java
@@ -1,4 +1,5 @@
 package com.xworkz.singletonimplementation;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -8,23 +9,23 @@ import com.xworkz.singletonimplementation.util.ConnectionProvider;
 
 public class LaptopDaoImpl implements LaptopDao {
 
-	public void savelaptopRecord(int id,String brand,String color,double price) {
+	public void savelaptopRecord(int id, String brand, String color, double price) {
 		System.out.println("invoked savelaptopRecord");
 		String insertQuery = "INSERT into  Laptop  VALUE(?,?,?,?)";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			//pass the query
+			Connection connection = ConnectionProvider.getConnection();
+			// pass the query
 			PreparedStatement preparedstatement = connection.prepareStatement(insertQuery);
-			//set the values using setters
+			// set the values using setters
 			preparedstatement.setInt(1, id);
-			preparedstatement.setString(2,brand);
-			preparedstatement.setString(3,color);
-			preparedstatement.setDouble(4,price);
+			preparedstatement.setString(2, brand);
+			preparedstatement.setString(3, color);
+			preparedstatement.setDouble(4, price);
 			preparedstatement.executeUpdate();
 			System.out.println("Laptop Record is Saved Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -32,20 +33,20 @@ public class LaptopDaoImpl implements LaptopDao {
 
 	}
 
-	public void updateBrand(String updateBrand ) {
+	public void updateBrand(String updateBrand) {
 		System.out.println("Invoked UpdateBrandRecord");
 		String updateQuery = "UPDATE Laptop set Brand = ? where id = 5";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			//pass the query
-			PreparedStatement preparedstatement = connection.prepareStatement(updateQuery );
-			//set the values using setters
-			preparedstatement.setString(1,updateBrand);
+			Connection connection = ConnectionProvider.getConnection();
+			// pass the query
+			PreparedStatement preparedstatement = connection.prepareStatement(updateQuery);
+			// set the values using setters
+			preparedstatement.setString(1, updateBrand);
 			preparedstatement.executeUpdate();
 			System.out.println("Laptop Update is Saved Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -58,15 +59,15 @@ public class LaptopDaoImpl implements LaptopDao {
 		String deleteQuery = "delete from laptop WHERE id=?";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			//pass the query
-			PreparedStatement preparedstatement = connection.prepareStatement(deleteQuery );
-			//set the values using setters
+			Connection connection = ConnectionProvider.getConnection();
+			// pass the query
+			PreparedStatement preparedstatement = connection.prepareStatement(deleteQuery);
+			// set the values using setters
 			preparedstatement.setInt(1, 1);
 			preparedstatement.executeUpdate();
 			System.out.println("Laptop from id deleted Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -79,15 +80,15 @@ public class LaptopDaoImpl implements LaptopDao {
 		String readSingleRecordById = "SELECT * FROM Laptop WHERE  id=?";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			//pass the query
-			PreparedStatement preparedstatement = connection.prepareStatement(readSingleRecordById );
-			//set the values using setters
+			Connection connection = ConnectionProvider.getConnection();
+			// pass the query
+			PreparedStatement preparedstatement = connection.prepareStatement(readSingleRecordById);
+			// set the values using setters
 			preparedstatement.setInt(1, id);
 			preparedstatement.executeQuery();
 			System.out.println("Single Line Record of laptop by ID Readed Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -95,44 +96,51 @@ public class LaptopDaoImpl implements LaptopDao {
 
 	}
 
-	//public void readAllRecordById(int id) {
-	//System.out.println("invoked readAllRecordById");
-	//String readAllRecordById = "SELECT * laptop WHERE id=? ";
-
-
-	//try {
-	//Connection connection= ConnectionProvider.getConnection();
-	//PreparedStatement preparedstatement = connection.prepareStatement(readAllRecordById);
-	//*boolean isRowPresent = resultSet.next();
-	//*while(isRowPresent)
-	//preparedstatement.setInt(1, id);
-	//preparedstatement.executeQuery();
-	//	System.out.println("id "+ preparedstatement.getResultSet().getInt(1));
-	//System.out.println("Brand "+ preparedstatement.getResultSet().getString(2));
-	//System.out.println("Color "+ preparedstatement.getResultSet().getString(3));
-	//System.out.println("Price "+ preparedstatement.getResultSet().getDouble(4));
-	//System.out.println("All Query Records of laptop by ID Readed Sucessfully");
-
-	//}catch (SQLException sqlException) {
-	//	System.out.println("Inside catch black Exception");
-	//System.out.println(sqlException.getMessage());
-	//sqlException.printStackTrace();
-	//}
-
-	//}
-	public void readlaptopPriceById(int id) {
-		System.out.println("invoked readlaptopPriceById");
-		String readlaptopPriceById="select price FROM Laptop WHERE id=?";
+	public void readAllRecord() {
+		System.out.println("invoked readAllRecordById");
+		String readAllRecord = "SELECT * From laptop.laptop ";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			PreparedStatement preparedstatement = connection.prepareStatement(readlaptopPriceById );
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement preparedstatement = connection.prepareStatement(readAllRecord);
+			// *boolean isRowPresent = resultSet.next();
+			// *while(isRowPresent)
+			ResultSet resultset = preparedstatement.executeQuery(readAllRecord);
+
+			while (resultset.next()) {
+				System.out.println("id " + preparedstatement.getResultSet().getInt(1));
+				System.out.println("Brand " + preparedstatement.getResultSet().getString(2));
+				System.out.println("Color " + preparedstatement.getResultSet().getString(3));
+				System.out.println("Price " + preparedstatement.getResultSet().getDouble(4));
+				System.out.println("Ram " + preparedstatement.getResultSet().getInt(5));
+				System.out.println("Processor " + preparedstatement.getResultSet().getString(6));
+				System.out.println("Is_Camera_Present " + preparedstatement.getResultSet().getBoolean(7));
+				System.out.println("Screen_Size " + preparedstatement.getResultSet().getString(8));
+				System.out.println("Weight " + preparedstatement.getResultSet().getInt(9));
+				System.out.println("Is_Cd_Supported " + preparedstatement.getResultSet().getBoolean(10));
+				System.out.println("All Query Records of laptop by ID Readed Sucessfully");
+			}
+		} catch (SQLException sqlException) {
+			System.out.println("Inside catch black Exception");
+			System.out.println(sqlException.getMessage());
+			sqlException.printStackTrace();
+		}
+
+	}
+
+	public void readlaptopPriceById(int id) {
+		System.out.println("invoked readlaptopPriceById");
+		String readlaptopPriceById = "select price FROM Laptop WHERE id=?";
+
+		try {
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement preparedstatement = connection.prepareStatement(readlaptopPriceById);
 			preparedstatement.setInt(1, id);
 			preparedstatement.executeQuery();
-			//*System.out.println("Price "+ preparedstatement.getResultSet().getDouble(4));
+			// *System.out.println("Price "+ preparedstatement.getResultSet().getDouble(4));
 			System.out.println("Laptop Price by ID is Readed  Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -141,18 +149,18 @@ public class LaptopDaoImpl implements LaptopDao {
 
 	public void readLaptopBrandById(int id) {
 		System.out.println("invoked readLaptopBrandById");
-		String readLaptopBrandById="select Brand FROM Laptop WHERE id=?";
+		String readLaptopBrandById = "select Brand FROM Laptop WHERE id=?";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			PreparedStatement preparedstatement = connection.prepareStatement(readLaptopBrandById );
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement preparedstatement = connection.prepareStatement(readLaptopBrandById);
 			preparedstatement.setInt(1, id);
 			preparedstatement.executeQuery();
 
-			//*System.out.println("Brand "+ preparedstatement.getResultSet().getDouble(3));
+			// *System.out.println("Brand "+ preparedstatement.getResultSet().getDouble(3));
 			System.out.println("Laptop Brand by ID is Readed  Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -161,16 +169,16 @@ public class LaptopDaoImpl implements LaptopDao {
 
 	public void readLaptopColorById(int id) {
 		System.out.println("readLaptopColorById");
-		String readLaptopColorById="select Color FROM Laptop WHERE id=?";
+		String readLaptopColorById = "select Color FROM Laptop WHERE id=?";
 
 		try {
-			Connection connection= ConnectionProvider.getConnection();
-			PreparedStatement preparedstatement = connection.prepareStatement(readLaptopColorById );
+			Connection connection = ConnectionProvider.getConnection();
+			PreparedStatement preparedstatement = connection.prepareStatement(readLaptopColorById);
 			preparedstatement.setInt(1, id);
 			preparedstatement.executeQuery();
 			System.out.println("Laptop Color FROM Id is Readed  Sucessfully");
 
-		}catch (SQLException sqlException) {
+		} catch (SQLException sqlException) {
 			System.out.println("Inside catch black Exception");
 			System.out.println(sqlException.getMessage());
 			sqlException.printStackTrace();
@@ -178,16 +186,4 @@ public class LaptopDaoImpl implements LaptopDao {
 
 	}
 
-	@Override
-	public void readAllRecordById(int id) {
-		// TODO Auto-generated method stub
-
-	}
-
-
-
-
-
 }
-
-
